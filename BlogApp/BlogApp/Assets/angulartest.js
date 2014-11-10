d3da4a4760c1ca7d10a9d4b26b56ad8b5fdc9984
unknown <burtonwu256@gmail.com>
2014-11-10 01:32:32 -0500

    adding angular testApp

---
+++ BlogApp/BlogApp/Assets/angulartest.js
@@ -0,0 +1,24 @@
+﻿angular.module('testApp', [])
+    .factory('testFactory', function () {
+        var secretData = ['num1', 'num2'];
+        factoryObj = {};
+        factoryObj.secrets = {
+            get: function () { return secretData; },
+            set: function (data) { secretData.push(data); }
+        };
+        factoryObj.testFunction = function (message) { return message + " testFunction"; }
+        return factoryObj;
+    })
+    .filter('reverse', function () {
+        return function(text)
+        {
+            return text.split("").reverse().join("");
+        }
+    })
+    .controller('testCtor', ['$scope','testFactory',function ($scope, testFactory) {
+        $scope.data = {
+            message: 'hello',
+            func2 : function() { return "func2"; }
+        }
+    }])
+    
